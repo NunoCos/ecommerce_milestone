2ba304baefc6e037af804d0f65f393aa3c1fa90e
NunoCos <nuno18_9@hotmail.com>
2021-08-30 23:25:28 +0000

    add shopping_app and templates

--- shopping/settings.py
+++ shopping/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'django_countries',
+    'shopping_app',
 ]
 
 MIDDLEWARE = [
---
+++ shopping_app/shopping_app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+from multiselectfield import MultiSelectField
+from django.shortcuts import reverse
+from django.conf import settings
+from django_countries.fields import CountryField
+import os
+# Create your models here.
+CATEGORY_CHOICES = (
+    ("Women", "Women"),
+    ("Men", "Men"),
+    ("Kids", "Kids"),
+    ("Accessories", "Accessories"),
+    ("Cosmetics", "Cosmetics"),
+)
+
+ADMIN_CHOICES = (
+    ("admin","admin"),
+)
+
+SIZE_CHOICES = (
+    ("S","S"),
+    ("M","M"),
+    ("L","L"),
+    ("XL","XL"),
+)
